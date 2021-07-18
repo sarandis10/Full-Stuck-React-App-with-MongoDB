38a5c9fc715adb247b4d5b56d99b9270088347a4
sarandis10 <43549151+sarandis10@users.noreply.github.com>
2021-07-18 15:31:30 +0100

    Add files via upload

---
+++ db/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    username: 'emile',
+    email: 'emile@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass'
+  },
+  {
+    username: 'cristina',
+    email: 'cristina@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass'
+  },
+  {
+    username: 'taras',
+    email: 'taras@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass'
+  },
+  {
+    username: 'saratis',
+    email: 'sarantis@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass'
+  }
+]
